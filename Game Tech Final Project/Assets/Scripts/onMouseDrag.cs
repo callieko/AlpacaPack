934f956176db54853e1085562525174dd8c0c812
Linh Nguyen <linh-nguyen@utexas.edu>
2017-11-30 17:42:05 -0600

    Added appropriate buttons to Item Creator. Added basic script to allow for dragging items on screen.

---
+++ Game Tech Final Project/Assets/Scripts/onMouseDrag.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onMouseDrag : MonoBehaviour {
+
+	float distance = 10;
+
+	// On mouse drag (https://www.youtube.com/watch?v=pK1CbnE2VsI)
+	void OnMouseDrag() {
+		Vector3 mousePos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+		Vector3 objectPos = Camera.main.ScreenToWorldPoint (mousePos);
+
+		transform.position = objectPos;
+	}
+
+}
