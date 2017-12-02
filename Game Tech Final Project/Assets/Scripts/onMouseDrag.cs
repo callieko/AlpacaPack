737d8b96b6ee03c7a8404b95c6cae53d5e059547
Linh Nguyen <linh-nguyen@utexas.edu>
2017-12-01 22:50:00 -0600

    Have some form of code based on onMouseDrag for moving, rotating, scaling. Need to be refactored and fixed to work smoothly in all cases. Currently have to comment which mode (move, rotate, scale) that you want. Need to change this in later iterations based on buttons

--- Game Tech Final Project/Assets/Scripts/onMouseDrag.cs
+++ Game Tech Final Project/Assets/Scripts/onMouseDrag.cs
@@ -4,20 +4,37 @@ using UnityEngine;
 
 public class onMouseDrag : MonoBehaviour {
 
-	public GameObject selectedMode = null;
 	float distance = 10;
+	private float rotationSpeed = 20;
+	private float yaw = 0.0f;
+	private float pitch = 0.0f;
+	float sizingFactor = 1.2f;
 
-	// On mouse drag (https://www.youtube.com/watch?v=pK1CbnE2VsI)
+	// Move object: (https://www.youtube.com/watch?v=pK1CbnE2VsI)
+	// Rotate object: (https://www.youtube.com/watch?v=S3pjBQObC90)
+	//                (https://gamedev.stackexchange.com/questions/104693/how-to-use-input-getaxismouse-x-y-to-rotate-the-camera)
+	// Scaling object: (https://stackoverflow.com/questions/19034528/game-in-unity3d-scaling-objects-with-mouse-in-c-sharp)
 	void OnMouseDrag() {
+		// CODE FOR MOVEMENT
 		Vector3 mousePos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
-		Quaternion mouseRot = new Quaternion (Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z, distance);
 		Vector3 objectPos = Camera.main.ScreenToWorldPoint (mousePos);
-
 		transform.position = objectPos;
-		transform.rotation = mouseRot;
+
+		// CODE FOR ROTATION
+//		yaw += Input.GetAxis("Mouse X") * rotationSpeed;
+//		pitch -= Input.GetAxis("Mouse Y") * rotationSpeed;
+//		transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+//		-----
+//		transform.RotateAround(Vector3.up, -rotationX);
+//		transform.RotateAround(Vector3.right, rotationY);
+
+		// CODE FOR SCALING
+//		Vector3 v3Scale = new Vector3(sizingFactor, sizingFactor, sizingFactor);
+//		transform.localScale = Vector3.Lerp(transform.localScale, v3Scale, Time.deltaTime);
 	}
 
-	public void GetCurrentMode (GameObject currentMode) {
-		selectedMode = currentMode;
-	}
+
+
+
+
 }
