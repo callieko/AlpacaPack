2755aaa65268e6e10bc271b77b0497427a126c30
Linh Nguyen <linh-nguyen@utexas.edu>
2017-12-01 19:56:54 -0600

    Adding empty files for future changes

---
+++ Game Tech Final Project/Assets/Scripts/MoveManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveManager : MonoBehaviour {
+
+	public GameObject sphere;
+	float distance = 10;
+
+	// On mouse drag (https://www.youtube.com/watch?v=pK1CbnE2VsI)
+	void OnMouseDrag() {
+		Vector3 mousePos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+		Vector3 objectPos = Camera.main.ScreenToWorldPoint (mousePos);
+
+		sphere.transform.position = objectPos;
+	}
+
+}
--- Game Tech Final Project/Assets/Scripts/onMouseDrag.cs
+++ Game Tech Final Project/Assets/Scripts/onMouseDrag.cs
@@ -4,38 +4,20 @@ using UnityEngine;
 
 public class onMouseDrag : MonoBehaviour {
 
-	public GameObject currentMode
-
-	private GameObject currentMenu = null;
-
+	public GameObject selectedMode = null;
 	float distance = 10;
 
 	// On mouse drag (https://www.youtube.com/watch?v=pK1CbnE2VsI)
 	void OnMouseDrag() {
 		Vector3 mousePos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+		Quaternion mouseRot = new Quaternion (Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z, distance);
 		Vector3 objectPos = Camera.main.ScreenToWorldPoint (mousePos);
 
 		transform.position = objectPos;
+		transform.rotation = mouseRot;
 	}
 
-	/* A function that is called by button that changes what menu is being shown */
-	public void ChangeMenu (GameObject nextMenu) {
-		//Index does not refer to any menu
-		if (nextMenu == null)
-			return;
-
-		//Hide the current menu, if there is one
-		if (currentMenu != null && currentMenu != nextMenu)
-			currentMenu.SetActive (false);
-
-		//Toggle activation of requested menu, allows for menu to be hidden by pressing the button
-		//for this menu while it is active
-		nextMenu.SetActive (!(nextMenu.activeInHierarchy));
-
-		//Set currentMenu
-		if (nextMenu.activeSelf)
-			currentMenu = nextMenu;
-		else
-			currentMenu = null;
+	public void GetCurrentMode (GameObject currentMode) {
+		selectedMode = currentMode;
 	}
 }
