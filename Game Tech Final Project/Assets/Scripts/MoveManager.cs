4166beb90e1dbc77cf202a1722f008cbb7cbbfc7
Linh Nguyen <linh-nguyen@utexas.edu>
2017-12-01 22:20:23 -0600

    Moved files to same folder

--- Game Tech Final Project/Assets/Scripts/MoveManager.cs
+++ Game Tech Final Project/Assets/Scripts/MoveManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MoveManager : MonoBehaviour {
 
-	public GameObject sphere;
 	float distance = 10;
 
 	// On mouse drag (https://www.youtube.com/watch?v=pK1CbnE2VsI)
@@ -12,7 +11,7 @@ public class MoveManager : MonoBehaviour {
 		Vector3 mousePos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
 		Vector3 objectPos = Camera.main.ScreenToWorldPoint (mousePos);
 
-		sphere.transform.position = objectPos;
+		transform.position = objectPos;
 	}
 
 }
