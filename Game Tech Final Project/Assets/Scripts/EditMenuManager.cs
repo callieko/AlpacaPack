8d7681d2779111981b75c68416794a9978c1a19b
Linh Nguyen <linh-nguyen@utexas.edu>
2017-12-04 21:24:59 -0600

    Cleaned up onMouseDrag code for movement, rotation, and scaling of objects by button. Must enter edit mode and click an edit button type before you can mess with objects on screen

--- Game Tech Final Project/Assets/Scripts/EditMenuManager.cs
+++ Game Tech Final Project/Assets/Scripts/EditMenuManager.cs
@@ -6,26 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class EditMenuManager : MonoBehaviour {
 
-	private GameObject currentEditMode = null;
+	public EditMode currentEditMode;
 
-	/* A function that is called by button that changes what edit mode is activated */
-	public void ChangeEditMode (GameObject nextEditMode) {
-		//Index does not refer to any menu
-		if (nextEditMode == null)
-			return;
+	public enum EditMode
+	{
+		None,
+		Move,
+		Rotate,
+		Scale,
+		Snap,
+		Remove
+	}
 
-		//Hide the current edit mode, if there is one
-		if (currentEditMode != null && currentEditMode != nextEditMode)
-			currentEditMode.SetActive (false);
+	void Start () {
+		currentEditMode = EditMode.None;
+	}
 
-		//Toggle activation of requested menu, allows for menu to be hidden by pressing the button
-		//for this menu while it is active
-		nextEditMode.SetActive (!(nextEditMode.activeInHierarchy));
-
-		//Set currentEditMode
-		if (nextEditMode.activeSelf)
-			currentEditMode = nextEditMode;
-		else
-			currentEditMode = null;
+	public void ChangeEditMode (int nextEditMode) {
+		print ("The editMode is: " + currentEditMode.ToString());
+		currentEditMode = (EditMode)nextEditMode;
 	}
 }
--- Game Tech Final Project/Assets/Scripts/onMouseDrag.cs
+++ Game Tech Final Project/Assets/Scripts/onMouseDrag.cs
@@ -4,60 +4,54 @@ using UnityEngine;
 
 public class onMouseDrag : MonoBehaviour {
 
-	float distance = 10;
-	private float rotationSpeed = 20;
+	public EditMenuManager editMenuManager;
+	// Variables for move
+	private float distance = 10f;
+	// Variables for rotation
+	private float rotationSpeed = 20f;
 	private float yaw = 0.0f;
 	private float pitch = 0.0f;
-	float sizingFactor = 2f;
-	KeyCode currentKey = KeyCode.M;
-	float starting = 0;
+	// Variables for scale
+	private float starting = 0f;
+	private float shrinkScale = 0.5f;
+	private float enlargeScale = 2f;
+
+	void Start () {
+		starting = Input.mousePosition.x;
+	}
 
 	// Move object: (https://www.youtube.com/watch?v=pK1CbnE2VsI)
 	// Rotate object: (https://www.youtube.com/watch?v=S3pjBQObC90)
 	//                (https://gamedev.stackexchange.com/questions/104693/how-to-use-input-getaxismouse-x-y-to-rotate-the-camera)
 	// Scaling object: (https://stackoverflow.com/questions/19034528/game-in-unity3d-scaling-objects-with-mouse-in-c-sharp)
 	void OnMouseDrag() {
-		if (starting == 0) {
-			starting = Input.mousePosition.x;
-		}
-		// CODE FOR MOVEMENT
-		if (Input.GetKeyDown (KeyCode.M)) {
-			currentKey = KeyCode.M;
-		} else if (Input.GetKeyDown (KeyCode.R)) {
-			currentKey = KeyCode.R;
-		} else if (Input.GetKeyDown (KeyCode.S)) {
-			currentKey = KeyCode.S;
-		} else if (Input.GetKeyDown (KeyCode.D)) {
-			currentKey = KeyCode.D;
-		}
-
-		print ("Current key is: " + currentKey);
-
-		if (currentKey == KeyCode.M) {
-			Vector3 mousePos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
-			Vector3 objectPos = Camera.main.ScreenToWorldPoint (mousePos);
-			transform.position = objectPos;
-		} else if (currentKey == KeyCode.R) {
-			currentKey = KeyCode.R;
-			yaw += Input.GetAxis("Mouse X") * rotationSpeed;
-			pitch -= Input.GetAxis("Mouse Y") * rotationSpeed;
-			transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
-		} else if (currentKey == KeyCode.S) {
-			currentKey = KeyCode.S;
-
-			if (starting > Input.mousePosition.x) {
-				Vector3 v3Scale = new Vector3 (0.5f, 0.5f, 0.5f);
-				transform.localScale = Vector3.Lerp (transform.localScale, v3Scale, Time.deltaTime);
-			} else {
-				Vector3 v3Scale = new Vector3 (sizingFactor, sizingFactor, sizingFactor);
-				transform.localScale = Vector3.Lerp (transform.localScale, v3Scale, Time.deltaTime);
-			}
-			starting = Input.mousePosition.x;
+		switch (editMenuManager.currentEditMode) {
+			case EditMenuManager.EditMode.None:
+				break;
+			case EditMenuManager.EditMode.Move:
+				Vector3 mousePos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+				Vector3 objectPos = Camera.main.ScreenToWorldPoint (mousePos);
+				transform.position = objectPos;
+				break;
+			case EditMenuManager.EditMode.Rotate:
+				yaw += Input.GetAxis("Mouse X") * rotationSpeed;
+				pitch -= Input.GetAxis("Mouse Y") * rotationSpeed;
+				transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+				break;
+			case EditMenuManager.EditMode.Scale:
+				if (starting > Input.mousePosition.x) {
+					Vector3 v3Scale = new Vector3 (shrinkScale, shrinkScale, shrinkScale);
+					transform.localScale = Vector3.Lerp (transform.localScale, v3Scale, Time.deltaTime);
+				} else if (starting < Input.mousePosition.x) {
+					Vector3 v3Scale = new Vector3 (enlargeScale, enlargeScale, enlargeScale);
+					transform.localScale = Vector3.Lerp (transform.localScale, v3Scale, Time.deltaTime);
+				}
+				starting = Input.mousePosition.x;
+				break;
+			case EditMenuManager.EditMode.Snap:
+				break;
+			case EditMenuManager.EditMode.Remove:
+				break;
 		}
 	}
-
-
-
-
-
 }
